9e4ea4a370e2ef1fe720d0b636c7be692fbb5763
michealcoleyoung <coleyoungmusic@gmail.com>
2021-01-01 19:58:22 -0600

    Set art_detail url name to image name

--- art/gallery/models.py
+++ art/gallery/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Gallery(models.Model):
     gallery_image = models.ImageField(upload_to='images/')
-    image_name = models.CharField(max_length=30)
+    image_name = models.CharField(max_length=30, primary_key=True)
     image_description = models.TextField()
 
     class Meta:
--- art/gallery/urls.py
+++ art/gallery/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('art/', views.art, name='art'),
     path('contact/', views.contact, name='contact'),
-    path('art/<int:pk>/', views.art_detail, name='art_detail'),
+    path('art/<str:pk>/', views.art_detail, name='art_detail'),
 ]
 
 
